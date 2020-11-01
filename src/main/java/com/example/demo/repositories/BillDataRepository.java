1e5add497948fba47fce9da12983f3680a7061c6
quangminh1407 <51909064+quangminh1407@users.noreply.github.com>
2020-11-01 12:32:43 +0700

    minor changes

--- src/main/java/com/example/demo/repositories/BillDataRepository.java
+++ src/main/java/com/example/demo/repositories/BillDataRepository.java
@@ -14,10 +14,10 @@ public interface BillDataRepository extends JpaRepository<BillData, Integer> {
     List<BillData> getBillDataByUserId(int id);
 
     @Query ("select b from BillData b where b.user.id = ?1 and b.category = ?2 and b.biller = ?3 " +
-                                            "and b.month != 0 and b.year != 0 and b.status = 1")
+                                            "and b.month != 0 and b.year != 0")
     List<BillData> getBillDataWithDate(int id, String category, String biller);
 
-    @Query ("select b from BillData b where b.user.id = ?1 and b.category = ?2 and b.biller = ?3 and b.status = 1")
+    @Query ("select b from BillData b where b.user.id = ?1 and b.category = ?2 and b.biller = ?3")
     List<BillData> getBillDataByUserIdAndCategoryAndBiller(int id, String category, String biller);
 
     @Query ("select b from BillData b where b.status = 1")
